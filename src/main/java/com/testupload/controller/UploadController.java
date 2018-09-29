3b2855e7a2a95289d5fc193e1084f186c1594010
曹海亮 <1050588656@qq.com>
2018-09-29 17:24:39 +0800

    Initial commit

---
+++ src/main/java/com/testupload/controller/UploadController.java
@@ -0,0 +1,44 @@
+package com.testupload.controller;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.*;
+import java.util.Objects;
+
+@RestController
+@RequestMapping("upload")
+public class UploadController {
+
+    @GetMapping("test")
+    public String hello() {
+        return "yes";
+    }
+
+    @PostMapping("img")
+    @CrossOrigin
+    public String uploadImg(@RequestParam("file") MultipartFile file, HttpServletRequest request) {
+//        String contextPath = request.getSession().getServletContext().getRealPath("/");
+        String contextPath = "D:/workspace/IdeaProjects/uploadImgs/";
+        if (!file.isEmpty()) {
+            try {
+                BufferedOutputStream out = new BufferedOutputStream(
+                        new FileOutputStream(new File((contextPath + file.getOriginalFilename()))));
+                System.out.println(contextPath + file.getOriginalFilename());
+                out.write(file.getBytes());
+                out.flush();
+                out.close();
+                System.out.println("yes");
+                return "yes";
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.out.println("no");
+                return "上传失败," + e.getMessage();
+            }
+        } else {
+            System.out.println("null");
+            return "上传失败，因为文件是空的.";
+        }
+    }
+}
